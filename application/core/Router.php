8a1b09b1db1bfe0cba821ea4cf9f9bedbc1f26b6
Vadim Gubeev <vadimka.good@yandex.ru>
2022-08-19 16:03:01 +0500

    Первый урок

---
+++ application/core/Router.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace application\core;
+
+class Router{
+    protected $routes = [];
+    protected $params = [];
+
+    function __construct(){
+
+    }
+
+    public function add(){
+
+    }
+
+    public function match(){
+
+    }
+
+    public function run(){
+        echo 'start';
+    }
+}
