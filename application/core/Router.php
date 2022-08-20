6e73766fdefde77ed082b34add917b24f599241d
Vadim Gubeev <vadimka.good@yandex.ru>
2022-08-20 15:54:37 +0500

    Добавление и проверка маршрутов

--- application/core/Router.php
+++ application/core/Router.php
@@ -7,18 +7,39 @@ class Router{
     protected $params = [];
 
     function __construct(){
-
+        $arr = require 'application/config/routes.php';
+        foreach ($arr as $key => $val) {
+            $this->add($key, $val);
+        }
     }
 
-    public function add(){
-
+    public function add($route, $params){ // функция добавления маршрутов
+       $route = '#^'.$route.'$#';
+       $this->routes[$route] = $params;
     }
 
-    public function match(){
-
+    public function match(){ //функция которая проверяет есть ли такой маршрут
+        $url = trim($_SERVER['REQUEST_URI'], '/');
+        foreach ($this->routes as $route => $params) {
+          if (preg_match($route, $url, $matches)) {
+              $this->params = $params;
+              return true;
+            }
+        }
+        return false;
     }
 
     public function run(){
-        echo 'start';
+      if ($this->match()){
+         $controller = 'application\controllers\\'.ucfirst($this->params['controller']).'Controller.php';
+         if (class_exists($controller)){
+             echo 'OK';
+         } else {
+            echo 'Не найден '. $controller;
+         }
+
+      } else {
+          echo 'Маршрут не найден';
+      }
     }
 }
